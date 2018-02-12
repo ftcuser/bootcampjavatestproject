c6bf966cdfc87415a6cfff350bb0fa98795f9b5a
ftcuser <rupesh.kumar@citizant.com>
2018-02-12 11:34:40 -0500

    updates to test

---
+++ src/test/java/com/citizant/todo/OrderTestCasesExecution.java
@@ -0,0 +1,51 @@
+package com.citizant.todo;
+
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+//import static org.junit.Assert.*;
+
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.After;
+//import org.junit.Before;
+import org.junit.FixMethodOrder;
+//import org.junit.Test;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+
+//import com.citizant.model.todo.Todo;
+//import com.citizant.todo.service.TodoService;
+
+//import static org.mockito.Mockito.*;
+
+//import java.sql.SQLException;
+
+//import com.citizant.order.bo.exception.BOException;
+//import com.citizant.order.dao.OrderDAO;
+//import com.citizant.order.dto.Order;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class OrderTestCasesExecution {
+	
+	@Test
+	public void firstTestCase()
+		{
+	 System.out.println("First Test case executed");
+	 
+	  }
+	@Test
+	public void thirdTestCase()
+		{
+	 System.out.println("Third Test case executed");
+	  }
+
+	@Test
+	public void secondTestCase()
+		{
+	 System.out.println("Second Test case executed");
+	  }
+	
+	
+
+}
