7e7ca919119936dfd40f1c8d5c0e8dc9d4e072c1
ftcuser <rupesh.kumar@citizant.com>
2018-03-22 10:47:06 -0400

    Add files via upload

---
+++ src/test/java/com/citizant/todo/OrderTest.java
@@ -0,0 +1,35 @@
+package com.citizant.todo.order; 
+
+ 
+ import static org.junit.Assert.*; 
+
+ 
+ import org.junit.Test; 
+
+ 
+ import com.citizant.model.order.Order; 
+ 
+ 
+public class OrderTest { 
+ 	 
+Order order = new Order(); 
+	 
+@Test 
+public void testTruncateAInFirst2Positions_Ainfirst2positions() { 
+	 
+		//AACD => CD ACD => CD CDEF=>CDEF CDAA => CDAA 
+ 		 		assertEquals( "CD", order.truncateAInFirst2Positions("AACD")); 
+ 		 
+	 //expected , actual 
+
+ 
+ 	}  	 
+ 	@Test 
+ 	public void testTruncateAInFirst2Positions_AinFirstposition(){ 
+ 
+ 	//	assertEquals( "CDA", helper.truncateAInFirst2Positions("ACD")); 
+		assertEquals( "CD", order.truncateAInFirst2Positions("ACD")); 
+ 		 
+} 
+ 
+}
